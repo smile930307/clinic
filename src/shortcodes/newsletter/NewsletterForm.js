3ac9490a4c9997bf32a1604862e6b117975a7018
smile930307 <koriumemura0307@gmail.com>
2020-09-22 16:10:47 +0200

    تغييرات فى اللون واضافة فورم التسجيل

--- src/shortcodes/newsletter/NewsletterForm.js
+++ src/shortcodes/newsletter/NewsletterForm.js
@@ -6,30 +6,29 @@ import "./style.scss";
 
 export default function NewsletterForm() {
   const [email, setEmail] = React.useState("");
-  const [listFields, setListFields] = React.useState({ name: "" });
   const [message, setMessage] = React.useState("");
   const [result, setResult] = React.useState({});
 
   React.useEffect(() => {
     if (result.result === "success") {
       setMessage(
-        "انتهينا تقريبا... نحتاج فقط إلى تأكيد بريدك الإلكتروني، اضغط على الرابط في رسالة البريد الإلكتروني التي أرسلناها إليك للتو."
+        "تمام جدا, احنا كدا خلصنا - فعلى يلا انضمامك لعيلتنا عن طريق الايميل اللى بعتناهولك دلوقتى"
       );
     }
 
     if (result.result === "error") {
       if (email) {
-        setMessage("لقد سبق لك الإشتراك في قائمة موبيرينا البريدية.");
+        setMessage("انتى اشتركتى معانا قبل كدا");
       } else {
-        setMessage("البريد الإلكتروني غير سليم.");
+        setMessage("ايميلك مش صح ياست الكل");
       }
     }
   }, [result.msg]);
 
-  function handleSubmit(email, listFields) {
+  function handleSubmit(email) {
     return async function(e) {
       e.preventDefault();
-      const result = await addToMailchimp(email, listFields);
+      const result = await addToMailchimp(email);
       setResult(result);
     };
   }
@@ -38,30 +37,20 @@ export default function NewsletterForm() {
     setEmail(e.target.value);
   }
 
-  function handleNameChange(e) {
-    setListFields({ ...listFields, name: e.target.value });
-  }
-
   return (
     <>
       {message && <blockquote className={result.result}>{message}</blockquote>}
       <div>
         <div className="newsletter-form">
-          <h4>القائمة البريدية</h4>
-          <form onSubmit={handleSubmit(email, { listFields })}>
+          <h4>اكتبى ايميلك علشان تنضمى لعيلتنا</h4>
+          <form onSubmit={handleSubmit(email)}>
             <input
               type="email"
               value={email}
               onChange={handleEmailChange}
-              placeholder="البريد الإلكتروني"
+              placeholder="ايميلك هنا ياست الكل"
             />
-            <input
-              type="text"
-              value={listFields.name}
-              onChange={handleNameChange}
-              placeholder="الاسم الكامل"
-            />
-            <button className="button">أريد الاشتراك</button>
+            <button className="button">اشتركى دلوقتى</button>
           </form>
         </div>
       </div>
