3edc7c6681dce03f62f9319b7cf068cb989c5cd9
smile930307 <koriumemura0307@gmail.com>
2020-09-24 20:44:42 +0200

    تغيير الصفحة الامامية

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,9 @@ import { graphql, Link } from "gatsby";
 /* import GitHubButton from 'react-github-btn'; */
 import Layout from "../layout";
 import PostListing from "../components/PostListing";
+import { NewsletterForm } from "../shortcodes";
+import shams from "../../content/images/shams.jpg";
+
 /* import ProjectListing from '../components/ProjectListing'; */
 /* import SimpleListing from '../components/SimpleListing'; */
 import SEO from "../components/SEO";
@@ -35,90 +38,28 @@ export default class Index extends Component {
 
         <div className="container">
           <div className="lead">
-            <div className="elevator">
-              <h1>الموقع الرسمى لـ د/ شمس</h1>
-              <p>
-                هنا يمكنك الاطلاع على ادق المعلومات الطبية, احدث بروتوكولات
-                العلاج وكذلك اهم النصائح التى تقدمها عيادة د/ شمس لتتمتع بصحة
-                أفضل. شفاكم الله وعافاكم
-              </p>
+            <div class="row">
+              <div class="col-md-8">
+                <div className="elevator">
+                  <h1>
+                    احصلى على ادق المعلومات الطبية فى النسا والتوليد واللى
+                    بشاركها فقط مع قائمة ايميلاتى
+                  </h1>
+                  <p>
+                    على الرغم من ان المدونة كويسة وهتلاقى فيها كل اللى محتاجاه -
+                    الا ان قائمة الايميلات - newsletter - اجمل
+                  </p>
 
-              <div className="social-buttons">
-                <div>
-                  <a
-                    href="https://www.facebook.com/%D8%B9%D9%8A%D8%A7%D8%AF%D8%A9-%D8%AF%D8%B4%D9%85%D8%B3-100330938395339/"
-                    title="حساب دكتور شمس على فيسبوك"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={facebookIcon} alt="حساب دكتور شمس على فيسبوك" />
-                  </a>
-                </div>
-
-                <div>
-                  <a
-                    href="https://twitter.com/clinics_shams"
-                    title="حساب دكتور شمس على تويتر"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={twitterIcon} alt="حساب دكتور شمس على تويتر" />
-                  </a>
-                </div>
-
-                <div>
-                  <a
-                    href="https://www.youtube.com/channel/UCikX-7rcNKEIo6oaqSGcvYg?view_as=subscriber"
-                    title="حساب دكتور شمس على يوتيوب"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={youtubeIcon} alt="حساب دكتور شمس على يوتيوب" />
-                  </a>
-                </div>
-
-                <div>
-                  <a
-                    href="https://www.pinterest.com/dr_shamsclinic/boards/"
-                    title="حساب دكتور شمس على pinterest"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      src={pinterestIcon}
-                      alt="حساب دكتور شمس على pinterest"
-                    />
-                  </a>
-                </div>
-
-                <div>
-                  <a
-                    href="mailto:shamsmohamed155@gmail.com"
-                    title="ايميل دكتور شمس على gmail"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={mailtoIcon} alt="ايميل دكتور شمس على gmail" />
-                  </a>
+                  <div className="container">
+                    <NewsletterForm />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="newsletter-section">
-              <img
-                src={emailList}
-                className="newsletter-avatar"
-                alt="دكتور شمس"
-              />
-              <div>
-                <h3>اتصل بالجديد دائما</h3>
-                <p>
-                  اشترك في قائمة د/ شمس البريدية لتتمكن من البقاء على اتصال
-                  دائم, وكذلك لمتابعة كل جديد يتم نشره من معلومات طبية, مستحضرات
-                  دوائية, طرق علاجية وكذلك متابعة دورية مباشرة لحالتك.
-                </p>
-                <a className="button" href="/newsletter">
-                  اشترك الان
-                </a>
+
+              <div class="col-md-4">
+                <div className="landingPage-image">
+                  <img src={shams} alt="د/ محمد شمس" />
+                </div>
               </div>
             </div>
           </div>
